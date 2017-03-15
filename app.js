9b07b7c2c9bfb4a99df5586aa19bf523af020368
karanagarwal17 <karanagarwal1702@gmail.com>
2017-03-15 21:49:37 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var count = 0;
-var stream;
+
+app.use(express.static(__dirname));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
   //res.send('<h1>Live Worldmap Visualisation</h1>');
 });
 
-http.listen( process.env.PORT, function(){
-  console.log('listening on ' + process.env.PORT);
+http.listen( process.env.PORT || 3000, function(){
+  console.log('listening on ' + process.env.PORT || 3000);
 });
 
 var client = new Twitter({
@@ -26,7 +27,6 @@ io.on('connection', function(socket){
   console.log('a user connected ' + count);
   count++;
   client.stream('statuses/filter', {locations: '-180,-90,180,90'}, function(stream) {
-    this.stream = stream;
   	stream.on('data', function(data) {
   		if (data.coordinates) {
   			console.log(data.coordinates.coordinates);
@@ -41,8 +41,13 @@ io.on('connection', function(socket){
     stream.on('error', function(error) {
       throw error;
     });
-  });
-  socket.on('disconnect', function(){
-      stream.destroy();
+    socket.on('disconnect', function(){
+      console.log('user disconnected ' + count);
+      count--;
+
+      if(count == 0){
+        stream.destroy();
+      }
+    });
   });
 });
