61bb0ade03706fa54efff18dd05e447501dc9620
karanagarwal17 <karanagarwal1702@gmail.com>
2017-03-15 20:16:31 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var count = 0;
 
 app.get('/', function(req, res){
   //res.sendFile(__dirname + '/custom.geo.json');
@@ -14,13 +15,6 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
 var client = new Twitter({
 	consumer_key: process.env.consumer_key,
 	consumer_secret: process.env.consumer_secret,
@@ -28,19 +22,30 @@ var client = new Twitter({
 	access_token_secret: process.env.access_token_secret
 });
 
-client.stream('statuses/filter', {locations: '-180,-90,180,90'}, function(stream) {
-	stream.on('data', function(data) {
-		if (data.coordinates) {
-			console.log(data.coordinates.coordinates);
-      var r = Math.sqrt(data.user.followers_count);
-			console.log(r);
-					io.emit('twitter', {
-						coordinates: data.coordinates,
-						radius: r
-					});
-				}
-			});
-  stream.on('error', function(error) {
-    throw error;
+io.on('connection', function(socket){
+  console.log('a user connected ' + count);
+  count++;
+  client.stream('statuses/filter', {locations: '-180,-90,180,90'}, function(stream) {
+  	stream.on('data', function(data) {
+  		if (data.coordinates) {
+  			console.log(data.coordinates.coordinates);
+        var r = Math.sqrt(data.user.followers_count);
+  			console.log(r);
+  					io.emit('twitter', {
+  						coordinates: data.coordinates,
+  						radius: r
+  					});
+  				}
+  			});
+    stream.on('error', function(error) {
+      throw error;
+    });
+    socket.on('disconnect', function(){
+      console.log('user disconnected ' + count);
+      count--;
+      if(count == 0){
+        stream.destroy();
+      }
+    });
   });
 });
